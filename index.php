cfbfe5169349a2f78aef4657046f185131594bdc
Facundo Conforti <facuconforti75@gmail.com>
2025-09-26 22:29:32 -0300

    feat: items y tarjetas
    Se agregaron los items
    Se agregaron las tarjetas
    Se creo un foreach para recorrer los items
    Se agrego estilos y se armaron las tarjetas

--- index.php
+++ index.php
@@ -1,12 +1,26 @@
+<?php
+include 'productos.php'; 
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Logitech</title>
     <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
-    <h1> HOLA </h1>
+    <?php include("header.html"); ?>
+    <h1>Catálogo</h1>
+    <div class="contenedor">
+        <?php foreach ($items as $item): ?>
+            <div class="tarjeta">
+                <img src="<?php echo $item["imagen"]; ?>" alt="<?php echo $item["titulo"]; ?>">
+                <h2><?php echo $item["titulo"]; ?></h2>
+                <p><?php echo $item["descripcion"]; ?></p>
+                <span><?php echo $item["categoria"]; ?></span>
+            </div>
+        <?php endforeach; ?>
+    </div>
 </body>
 </html>
---
+++ productos.php
@@ -0,0 +1,64 @@
+<?php
+
+$items = [
+[   
+    "id" => 1,
+    "titulo" => "Teclado G515",
+    "categoria" => "Teclado",
+    "descripcion" => "El teclado inalámbrico para juegos Logitech G515 LIGHTSPEED TKL ofrece un alto rendimiento y un diseño estético de perfil bajo.",
+    "imagen" => "img/teclado-g515-white.webp"
+],
+[
+    "id" => 2,
+    "titulo" => "Teclado G515",
+    "categoria" => "Teclado",
+    "descripcion" => "El teclado inalámbrico para juegos Logitech G515 LIGHTSPEED TKL ofrece un alto rendimiento y un diseño estético de perfil bajo.",
+    "imagen" => "img/teclado-g515-white.webp"
+],
+[
+    "id" => 3,
+    "titulo" => "Teclado G515",
+    "categoria" => "Teclado",
+    "descripcion" => "El teclado inalámbrico para juegos Logitech G515 LIGHTSPEED TKL ofrece un alto rendimiento y un diseño estético de perfil bajo.",
+    "imagen" => "img/teclado-g515-white.webp"
+],
+[   
+    "id" => 4,
+    "titulo" => "Teclado G515",
+    "categoria" => "Teclado",
+    "descripcion" => "El teclado inalámbrico para juegos Logitech G515 LIGHTSPEED TKL ofrece un alto rendimiento y un diseño estético de perfil bajo.",
+    "imagen" => "img/teclado-g515-white.webp"
+],
+[
+    "id" => 5,
+    "titulo" => "Teclado G515",
+    "categoria" => "Teclado",
+    "descripcion" => "El teclado inalámbrico para juegos Logitech G515 LIGHTSPEED TKL ofrece un alto rendimiento y un diseño estético de perfil bajo.",
+    "imagen" => "img/teclado-g515-white.webp"
+],
+[
+    "id" => 6,
+    "titulo" => "Teclado G515",
+    "categoria" => "Teclado",
+    "descripcion" => "El teclado inalámbrico para juegos Logitech G515 LIGHTSPEED TKL ofrece un alto rendimiento y un diseño estético de perfil bajo.",
+    "imagen" => "img/teclado-g515-white.webp"
+],
+[
+    "id" => 7,
+    "titulo" => "Teclado G515",
+    "categoria" => "Teclado",
+    "descripcion" => "El teclado inalámbrico para juegos Logitech G515 LIGHTSPEED TKL ofrece un alto rendimiento y un diseño estético de perfil bajo.",
+    "imagen" => "img/teclado-g515-white.webp"
+],
+[
+    "id" => 8,
+    "titulo" => "Teclado G515",
+    "categoria" => "Teclado",
+    "descripcion" => "El teclado inalámbrico para juegos Logitech G515 LIGHTSPEED TKL ofrece un alto rendimiento y un diseño estético de perfil bajo.",
+    "imagen" => "img/teclado-g515-white.webp"
+]
+
+
+];
+
+?>
