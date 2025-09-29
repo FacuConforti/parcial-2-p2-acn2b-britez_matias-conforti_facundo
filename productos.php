48885b0c39aa0a17dd783e65cf312f5dcd402086
Facundo Conforti <facuconforti75@gmail.com>
2025-09-29 15:21:42 -0300

    refactor: modificacion de items
    Se agregaron imagenes
    Se modificaron los items de productos
    Se verifico que se vea correctamente en la web

--- productos.php
+++ productos.php
@@ -10,52 +10,52 @@ $items = [
 ],
 [
     "id" => 2,
-    "titulo" => "Teclado G515",
+    "titulo" => "Teclado G915 X LIGHTSPEED",
     "categoria" => "Teclado",
-    "descripcion" => "El teclado inalámbrico para juegos Logitech G515 LIGHTSPEED TKL ofrece un alto rendimiento y un diseño estético de perfil bajo.",
-    "imagen" => "img/teclado-g515-white.webp"
+    "descripcion" => "Logitech G915 X LIGHTSPEED ofrece velocidad, precisión y personalización icónicas con un diseño elegante y ultrafino.",
+    "imagen" => "img/TecladoG915.webp"
 ],
 [
     "id" => 3,
-    "titulo" => "Teclado G515",
-    "categoria" => "Teclado",
-    "descripcion" => "El teclado inalámbrico para juegos Logitech G515 LIGHTSPEED TKL ofrece un alto rendimiento y un diseño estético de perfil bajo.",
-    "imagen" => "img/teclado-g515-white.webp"
+    "titulo" => "Auriculares G321",
+    "categoria" => "Auriculares",
+    "descripcion" => "Diseñados para ofrecer comodidad y rendimiento desde el primer momento. Los G321 LIGHTSPEED ofrecen comodidad y ligereza en un formato de solo 210gr.",
+    "imagen" => "img/AuricularesG321.webp"
 ],
 [   
     "id" => 4,
-    "titulo" => "Teclado G515",
-    "categoria" => "Teclado",
-    "descripcion" => "El teclado inalámbrico para juegos Logitech G515 LIGHTSPEED TKL ofrece un alto rendimiento y un diseño estético de perfil bajo.",
-    "imagen" => "img/teclado-g515-white.webp"
+    "titulo" => "Astro A30",
+    "categoria" => "Auriculares",
+    "descripcion" => "A30 Wireless combina la máxima flexibilidad, movilidad, estilo y comodidad en unos auriculares para juegos.",
+    "imagen" => "img/AstroA30.webp"
 ],
 [
     "id" => 5,
-    "titulo" => "Teclado G515",
-    "categoria" => "Teclado",
-    "descripcion" => "El teclado inalámbrico para juegos Logitech G515 LIGHTSPEED TKL ofrece un alto rendimiento y un diseño estético de perfil bajo.",
-    "imagen" => "img/teclado-g515-white.webp"
+    "titulo" => "VolanteG923",
+    "categoria" => "Volante",
+    "descripcion" => "Tecnología Force Feedback TRUEFORCE exclusiva con una conexión directa a la física de los juegos, para ofrecer realismo sin precedentes.",
+    "imagen" => "img/VolanteG923.webp"
 ],
 [
     "id" => 6,
-    "titulo" => "Teclado G515",
-    "categoria" => "Teclado",
-    "descripcion" => "El teclado inalámbrico para juegos Logitech G515 LIGHTSPEED TKL ofrece un alto rendimiento y un diseño estético de perfil bajo.",
-    "imagen" => "img/teclado-g515-white.webp"
+    "titulo" => "VolanteG29",
+    "categoria" => "Volante",
+    "descripcion" => "Diseñado para ofrecer la experiencia de conducción perfecta, con Force Feedback con dos motores, palancas de cambio de acero inoxidable y volante de cuero cosido a mano.",
+    "imagen" => "img/VolanteG29.webp"
 ],
 [
     "id" => 7,
-    "titulo" => "Teclado G515",
-    "categoria" => "Teclado",
-    "descripcion" => "El teclado inalámbrico para juegos Logitech G515 LIGHTSPEED TKL ofrece un alto rendimiento y un diseño estético de perfil bajo.",
-    "imagen" => "img/teclado-g515-white.webp"
+    "titulo" => "Mouse PRO X SUPERLIGHT 2c",
+    "categoria" => "Mouse",
+    "descripcion" => "El PRO X SUPERLIGHT 2c es un mouse compacto de 51 g que cuenta con el avanzado sensor HERO 2, robusta tecnología inalámbrica LIGHTSPEED e interruptores LIGHTFORCE.",
+    "imagen" => "img/MouseProXSuperlight2c.webp"
 ],
 [
     "id" => 8,
-    "titulo" => "Teclado G515",
-    "categoria" => "Teclado",
+    "titulo" => "Mouse G502 X PLUS",
+    "categoria" => "Mouse",
     "descripcion" => "El teclado inalámbrico para juegos Logitech G515 LIGHTSPEED TKL ofrece un alto rendimiento y un diseño estético de perfil bajo.",
-    "imagen" => "img/teclado-g515-white.webp"
+    "imagen" => "img/MouseG502XPLUS.webp"
 ]
 
 
